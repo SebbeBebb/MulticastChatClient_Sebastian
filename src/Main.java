6b3bf2af877fafef87b14237735e5881e8fc9a4f
Sebastian <sebastian.samuelsson@elev.ga.ntig.se>
2024-05-13 09:39:52 +0200

    update

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.net.*;
+import java.io.*;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception{
+        int portnumber = 8080;
+        if (args.length >= 1) {
+            portnumber = Integer.parseInt(args[0]);
+        }
+        MulticastSocket chatMulticastSocket = new MulticastSocket(portnumber);
+        InetAddress group = InetAddress.getByName("225.4.5.6");
+
+        chatMulticastSocket.joinGroup(group);
+        String msg = "";
+        System.out.println("Type message for server");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        msg = br.readLine();
+
+        DatagramPacket data = new DatagramPacket(msg.getBytes(), 0, msg.length(), group, portnumber);
+        chatMulticastSocket.send(data);
+        chatMulticastSocket.close();
     }
 }
